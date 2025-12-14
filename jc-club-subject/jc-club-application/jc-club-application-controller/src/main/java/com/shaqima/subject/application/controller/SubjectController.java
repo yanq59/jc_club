f7709a4b3a1fbaa2581eda220f48b8134f384f4f
qi <2920137898@qq.com>
2025-12-15 01:51:37 +0900

    能连数据库了 没用加密

--- jc-club-subject/jc-club-application/jc-club-application-controller/src/main/java/com/shaqima/subject/application/controller/SubjectController.java
+++ jc-club-subject/jc-club-application/jc-club-application-controller/src/main/java/com/shaqima/subject/application/controller/SubjectController.java
@@ -1,7 +1,11 @@
 package com.shaqima.subject.application.controller;
 
+import com.shaqima.subject.infra.basic.entity.SubjectCategory;
+import com.shaqima.subject.infra.basic.service.SubjectCategoryService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * 刷题controller
@@ -12,8 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SubjectController {
 
+    @Resource
+    private SubjectCategoryService subjectCategoryService;
+
     @GetMapping("/test")
     public String test(){
-        return "hello world";
+        SubjectCategory subjectCategory = subjectCategoryService.queryById(1L);
+        return subjectCategory.getCategoryName();
     }
 }
--- jc-club-subject/jc-club-starter/src/main/java/com/shaqima/subject/SubjectApplication.java
+++ jc-club-subject/jc-club-starter/src/main/java/com/shaqima/subject/SubjectApplication.java
@@ -1,5 +1,6 @@
 package com.shaqima.subject;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.shaqima")
+@MapperScan("com.shaqima.**.mapper") //想把dao的类扫描加载到spring容器中进来即让 MyBatis 自动为这些接口生成代理对象并交给 Spring 管理
 public class SubjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(SubjectApplication.class, args);
