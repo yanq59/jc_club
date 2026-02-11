10bb80e5d13ad19a145c3f0c1118a24a682e40f3
qi <2920137898@qq.com>
2026-02-11 19:55:51 +0900

    【题目分类】 增删改查

---
+++ jc-club-subject/jc-club-common/src/main/java/com/shaqima/subject/common/enums/CategroyTypeEnum.java
@@ -0,0 +1,8 @@
+package com.shaqima.subject.common.enums;
+
+/**
+* Created by IntelliJ IDEA.
+* @Author : qi
+* @create 2026/2/11 19:04
+*/public enum CategroyTypeEnum {
+}
---
+++ jc-club-subject/jc-club-common/src/main/java/com/shaqima/subject/common/enums/IsDeletedFlagEnum.java
@@ -0,0 +1,8 @@
+package com.shaqima.subject.common.enums;
+
+/**
+* Created by IntelliJ IDEA.
+* @Author : qi
+* @create 2026/2/11 18:48
+*/public enum IsDeletedFlagEnum {
+}
