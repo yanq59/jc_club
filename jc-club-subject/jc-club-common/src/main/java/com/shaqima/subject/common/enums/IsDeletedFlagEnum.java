bf5256c5e9187329d3b99dbc087b2bf9db34822d
qi <2920137898@qq.com>
2026-02-11 20:47:36 +0900

    【题目分类】 增删改查

--- jc-club-subject/jc-club-common/src/main/java/com/shaqima/subject/common/enums/IsDeletedFlagEnum.java
+++ jc-club-subject/jc-club-common/src/main/java/com/shaqima/subject/common/enums/IsDeletedFlagEnum.java
@@ -1,8 +1,35 @@
 package com.shaqima.subject.common.enums;
 
+import lombok.Getter;
+
 /**
-* Created by IntelliJ IDEA.
-* @Author : qi
-* @create 2026/2/11 18:48
-*/public enum IsDeletedFlagEnum {
+ * 删除状态枚举
+ *
+ * @Author : qi
+ * @create 2026/2/11 18:48
+ */
+@Getter
+public enum IsDeletedFlagEnum {
+
+    UN_DELETED(0, "未删除"),
+    DELETED(1, "已删除");
+
+    private final Integer code;
+    private final String message;
+
+    IsDeletedFlagEnum(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public static IsDeletedFlagEnum getByCode(int codeVal){
+        for (IsDeletedFlagEnum resultCodeEnum:IsDeletedFlagEnum.values()){
+            if (resultCodeEnum.code == codeVal){
+                return resultCodeEnum;
+            }
+
+        }
+        return null;
+    }
+
 }
--- jc-club-subject/jc-club-domain/src/main/java/com/shaqima/subject/domain/service/SubjectCategoryDomainService.java
+++ jc-club-subject/jc-club-domain/src/main/java/com/shaqima/subject/domain/service/SubjectCategoryDomainService.java
@@ -14,8 +14,16 @@ import java.util.List;
 public interface SubjectCategoryDomainService {
 
     void add(SubjectCategoryBO subjectCategoryBO);
-/**
+    /**
     * 查询岗位大类
- * */
+    * */
     List<SubjectCategoryBO> queryCategory(SubjectCategoryBO subjectCategoryBO);
+    /**
+     * 更新分类
+     * */
+    Boolean update(SubjectCategoryBO subjectCategoryBO);
+    /**
+     * 删除分类
+     * */
+    Boolean delete(SubjectCategoryBO subjectCategoryBO);
 }
